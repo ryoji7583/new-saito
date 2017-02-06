df675125002648742df173d7a235b0bf96ded4aa
kjm2086 <2086psp@gmail.com>
2017-02-06 16:33:51 +0900

    4－1－c

--- zaikokanri/WebContent/js/script.js
+++ zaikokanri/WebContent/js/script.js
@@ -21,12 +21,22 @@ function reloadList(){
         $list.append("<tr><th>商品名</th><th>個数</th><th>出荷</th></tr>");
 
         for(var i=0; i<reply.length; i++){
+            var $inputAmount = $("<input></input>").attr("type", "text");
+            var $exbtn = $("<button></button>").text("出荷");
+
+            //ボタンにイベントを登録
+            $exbtn.on('click', {sname:reply[i].name, $inputAmount}, function(e){
+                syukka(e.data.sname, e.data.$inputAmount.val());
+            });
+
             //表に追加
+            //商品名にリンクを追加する
             $list.append($("<tr></tr>").append(
-                $("<td></td>").text(reply[i].name),
-                $("<td></td>").text(reply[i].amount)
+                $("<td></td>").append($("<a></a>").text(reply[i].name)
+                        .attr("href", "history.html?item=" + reply[i].name)), 
+                $("<td></td>").text(reply[i].amount),
+                $("<td></td>").append($inputAmount, $exbtn)
             ));
         }
     });
 }
-
